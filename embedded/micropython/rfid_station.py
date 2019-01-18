bbbc2ac5a270dc72c6d730b8896c31a6dfe48d96
stephan-kashkarov <stephankashkarovofficial@gmail.com>
2019-01-18 19:57:01 +1100

    tried some micropython stuff to no avail

--- embedded/micropython/rfid_station.py
+++ embedded/micropython/rfid_station.py
@@ -1,4 +1,3 @@
-import network
 import machine
 import urequests
 import ujson
@@ -13,16 +12,6 @@ DEVICE_PASSWORD = "admin123"
 # Lib inits
 rfid = mfrc522.MFRC522(5, 7, 6, 1, 2)
 
-
-def connect_wifi(essid, password):
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect(essid, password)
-        while not wlan.isconnected():
-            pass
-    print('network config:', wlan.ifconfig())
 
 def get_class(class_id):
     data = {
@@ -45,8 +34,12 @@ def get_uid():
         if stat == rfid.OK:
             stat, raw_uid = rfid.anticoll()
             if stat == rfid.OK:
-                print(f"uid: {raw_uid}")
+                print("tag type: " + tag_type)
+                print("uid: " + raw_uid)
+                return raw_uid
             else:
                 print("Something went wrong")
         else:
             print("No Card maybe?")
+
+print("Hello!")
