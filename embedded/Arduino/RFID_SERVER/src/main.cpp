1445c3ed058c3b3cc94da14d7cfea8025911a622
Stephan-kashkarov <Stephankashkarovofficial@gmail.com>
2019-01-25 22:15:40 +1100

    worked on making the program store previous uid to ensure that it dosent send the same uid multiple times

--- embedded/Arduino/RFID_SERVER/src/main.cpp
+++ embedded/Arduino/RFID_SERVER/src/main.cpp
@@ -1,10 +1,12 @@
 /*
 RFID Server
+
 Description:
  * rfid server is a program designed to be run on the node mcu v2.
  * Its purpose is to talk to a server and send across rfid uids.
  * This was made to create a marking system for school teacher with
  * rfid enabled student ids. This will make marking really quick.
+
 Typical pin layout used:
  * -------------------------------------
  *             MFRC522      Expressif   
@@ -16,27 +18,67 @@ Typical pin layout used:
  * SPI MOSI    MOSI         11 / ICSP-4 
  * SPI MISO    MISO         12 / ICSP-1 
  * SPI SCK     SCK          13 / ICSP-3 
+
 Credits:
  * Written by Stephan kashkarov
  * In January, 2019
  * source: https://github.com/Stephan-kashkarov/QuickMark-RIFD/blob/master/embedded/Arduino/RFID_SERVER/src/main.cpp
 */
+
+// Lib lnclude
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <stdio.h>
+
 
 // Static defines
 #define RST_PIN 5
 #define SS_PIN  4
 
 // Variables
-byte* uid;
+std::vector<byte> uid;
+std::vector<byte> prev_uid;
 const char* ssid = "";
 const char* password = "";
 
 // Class init
 MFRC522 rfid(SS_PIN, RST_PIN);
+
+
+// General functions
+void printHex(std::vector<byte> buffer, byte size)
+{
+    for (byte i = 0; i < size; i++)
+    {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
+}
+
+bool check_equal_vector(std::vector<byte> a, std::vector<byte> b)
+{
+    // if (a.size() != b.size()) return false;
+
+    for(byte i = 0; i < a.size(); ++i)
+    {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
+void byte_vec_cpy(std::vector<byte> a, std::vector<byte> b)
+{
+    b.resize(a.size());
+    for (byte i = 0; i < a.size(); ++i)
+    {
+        b[i] = a[i];
+    }
+    printHex(a, a.size());
+    Serial.print("\n");
+    printHex(b, b.size());
+}
 
 // WiFi functions
 void wifi_init(const char* ssid, const char* password)
@@ -60,22 +102,21 @@ bool card_present()
     return true;
 }
 
-byte* card_get_uid()
+void card_get_uid(std::vector<byte>& uuid)
 {
     Serial.println("[RFID: Getting UID]");
     MFRC522::Uid uidObj = rfid.uid;
-    Serial.print("[RFID: Uid is: ");
+    uuid.resize(uidObj.size);
     for (byte i = 0; i < uidObj.size; i++)
     {
-        Serial.print(uidObj.uidByte[i] < 0x10 ? " 0" : " ");
-        Serial.print(uidObj.uidByte[i], HEX);
+        uuid[i] = uidObj.uidByte[i];
     }
-    Serial.println("]");
-    return rfid.uid.uidByte;
 }
 
+
+
 // Server Functions
-void send_server(byte* uid)
+void send_server(std::vector<byte> uuid)
 {
 
 }
@@ -96,10 +137,15 @@ void loop()
 {
     if (card_present())
     {
-        uid = card_get_uid();
-        Serial.println("[RFID: sending UID to server]");
-        send_server(uid);
-        uid = 0;
+        card_get_uid(uid);
+        printHex(uid, uid.size());
+        printHex(prev_uid, prev_uid.size());
+        if (!check_equal_vector(prev_uid, uid)){
+            Serial.println("[RFID: sending UID to server]");
+            send_server(uid);
+            byte_vec_cpy(uid, prev_uid);
+        }
+        // memcpy(&prev_uid[0], &uid[0], uid.size());
     }
     delay(50);
 }
