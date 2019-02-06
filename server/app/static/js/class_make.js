2e2cd27ad29e68b92f4a4455d5807c61e5614ea9
Stephan-kashkarov <stephankashkarovofficial@gmail.com>
2019-02-06 21:02:41 +1100

    tonnes of js work

--- server/app/static/js/class_make.js
+++ server/app/static/js/class_make.js
@@ -1,6 +1,7 @@
 $(function(){
     let students = []
-
+    let searchVal = "name"
+    $(".selected-students").hide()
 
     $(".student").on('mouseenter', function() {
         $(this).animate({
@@ -15,15 +16,43 @@ $(function(){
             backgroundColor: "#007bff",
         }, 100)
         $(this).removeClass("student").addClass("added-student")
-        students.push(parseInt($(this).find('.id').innerText()))
+        students.push(parseInt($(this).find('.id').text()))
     })
 
     $(".added-student").on('click', function(){
         $(this).animate({
-            backgroundColor: "#868e96",
+            backgroundColor: "#007bff",
         }, 100)
         $(this).removeClass("added-student").addClass("student")
-        let id = parseInt($(this).find('.id').innerText())
+        let id = parseInt($(this).find('.id').text())
         students.splice(students.indexOf(id), 1)
     })
+
+    $(".class-groups").on('click', function(e){
+        e.preventDefault()
+        $(".class-tabs").find(".active").removeClass("active")
+        $(this).addClass('active')
+        $('.search-students').toggle()
+        $('.selected-students').toggle()
+    })
+
+    $(".search-choose").on('click', function(e){
+        e.preventDefault()
+        $(".search-choose-menu").find('.active').removeClass('active')
+        $(this).addClass('active')
+        $('.search-name').text($(this).text())
+        switch ($(this).text()) {
+            case "Name":
+                searchVal = "name"
+                break;
+            case "Student ID":
+                searchVal = "id"
+                break;
+        
+            default:
+                break;
+        }
+    })
+
+
 })
